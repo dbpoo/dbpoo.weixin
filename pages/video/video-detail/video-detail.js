0b481305370da980decbd1ef64ee94384fb1f34d
weiwei <weiwei198328@gmail.com>
2017-07-06 10:29:11 +0800

    update

--- pages/video/video-detail/video-detail.js
+++ pages/video/video-detail/video-detail.js
@@ -3,7 +3,7 @@ var app = getApp();
 
 Page({
   data: {
-    'oldindex' : -1
+    curIndex: -1
   },
   onLoad: function (options) {
     wx.showLoading({
@@ -19,15 +19,9 @@ Page({
   },
   onTap: function (evt) {
     var index = evt.currentTarget.dataset.index;
-    var oldindex = this.data.oldindex;
-    if (index === this.oldindex) return;
-    if (oldindex >= 0) {
-      this.data.videoList[oldindex].isPlay = '';
-    }
-    this.data.videoList[index].isPlay = 'true';
+
     this.setData({
-      videoList: this.data.videoList,
-      oldindex : index
+      curIndex: index
     })
   }
 })
