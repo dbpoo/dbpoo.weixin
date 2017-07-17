258cad28ceb7a1022de4a21e0676a42184bfd6fe
weiwei <weiwei198328@gmail.com>
2017-07-17 18:22:41 +0800

    update

--- app.js
+++ app.js
@@ -7,13 +7,18 @@ App({
   },
   indexAPI: {
     swiper: 'https://static.ledo.com/api/swiper.js',
-    post: 'https://static.ledo.com/api/post.js',
+    hot: 'https://static.ledo.com/api/hot.js',
+    news: 'https://static.ledo.com/api/news.js',
+    announce: 'https://static.ledo.com/api/announce.js',
+    study: 'https://static.ledo.com/api/study.js',
     detail: 'https://static.ledo.com/api/detail.js',
     video: 'https://static.ledo.com/api/video.js',
     avatar: 'http://static.ledo.com/api/avatar.js?v=22',
     skill: 'https://static.ledo.com/api/people.js',
     search: 'https://static.ledo.com/api/post.js',
-    game: 'https://static.ledo.com/api/game.js?v=22'
+    game: 'https://static.ledo.com/api/game.js?v=22',
+    search: 'http://static.ledo.com/api/search.js?v=22',
+    searchkey: 'http://static.ledo.com/api/searchkey.js?v=33'
   },
   videoAPI: {
     lastestList: '',
--- pages/index/index.js
+++ pages/index/index.js
@@ -5,23 +5,19 @@ Page({
   data: {
     swiperList: [],
     postList: [],
-    indicatorDots: true,
-    autoplay: true,
-    interval: 3000,
-    duration: 500,
-    circular: true,
-    indicatorColor: 'rgba(255,255,255,.3)',
-    indicatorActiveColor: '#ffffff',
-    page: 1,
-    size: 20,
     hasMore: false,
     hasRefresh: true,
     loadTips: app.globalData.LOADING,
-    isShow: false,
     placeholder: '请输入游戏角色',
     searchValue: '',
     searchHotShow: true,
-    navcur: 0
+    navcur: 0,
+    start: 0,
+    count: 10,
+    total: 0,
+    isComplete: false,
+    isSearch: false,
+    keywords: ''
   },
   onLoad: function () {
     wx.showLoading({
@@ -29,9 +25,39 @@ Page({
     })
     this.getIndexData();
   },
+  onNav: function (evt) {
+    var index = parseInt(evt.currentTarget.dataset.id);
+    var url = '';
+    if (this.data.navcur == index) return;
+    this.setData({
+      navcur: index
+    })
+    url = this.getUrl(index);
+    util.http(url + '?start=0' + '&count=' + this.data.count, this.getPostList)
+  },
+  getUrl: function (index) {
+    switch (index) {
+      case 0:
+        return app.indexAPI.hot;
+        break;
+      case 1:
+        return app.indexAPI.news;
+        break;
+      case 2:
+        return app.indexAPI.announce;
+        break;
+      case 3:
+        return app.indexAPI.study;
+        break;
+      default:
+        return app.indexAPI.search;
+        break;
+    }
+  },
   getIndexData: function () {
-    util.http(app.indexAPI.swiper, this.getSwiperList)
-    util.http(app.indexAPI.post + '?nav=' + this.data.navcur, this.getPostList)
+    util.http(app.indexAPI.swiper, this.getSwiperList);
+    util.http(app.indexAPI.hot + '?start=' + this.data.start + '&count=' + this.data.count, this.getPostList);
+    util.http(app.indexAPI.searchkey, this.getSearchKey);
   },
   getSwiperList: function (res) {
     if (!res) return;
@@ -43,28 +69,85 @@ Page({
     wx.hideLoading();
     if (!res) return;
     this.setData({
+      start: 0,
+      isComplete: false,
+      hasMore: false,
+      loadTips: app.globalData.LOADING,
+      total: res.data.total,
       postList: res.data.postData
     })
   },
-  onPullDownRefresh: function () {
-    wx.stopPullDownRefresh()
-    wx.showLoading({
-      title: app.globalData.LOADING,
+  getSearchKey: function (res) {
+    this.setData({
+      hotkey: res.data.hotkey,
+      actorkey: res.data.actorkey
     })
-    setTimeout(() => {
-      this.getIndexData();
-    }, 2000)
+  },
+  searchkey: function (evt) {
+    this.data.keywords = evt.currentTarget.dataset.key;
+    this.setData({
+      searchValue: this.data.keywords
+    });
+    util.http(app.indexAPI.search + '?&keywords=' + this.data.keywords + 'start=0&count=' + this.data.count, this.getSearchList)
+  },
+  onPullDownRefresh: function () {
+    var index = this.data.navcur;
+    var url = this.getUrl(index);
+
+    util.http(url, this.refreshPostList)
+  },
+  refreshPostList: function (res) {
+    wx.stopPullDownRefresh()
+    if (!res) return;
+    this.setData({
+      total: res.data.total,
+      postList: res.data.postData
+    })
   },
   onReachBottom: function () {
+    var index = this.data.navcur;
+    var url = '';
     if (this.data.hasMore) return;
     this.setData({
       hasMore: true
     })
-    setTimeout(() => {
+    if (this.data.isSearch) {
+      url = this.getUrl();
+    } else {
+      url = this.getUrl(index);
+    }
+
+    if (this.data.isComplete) return;
+    this.data.start += this.data.count;
+    if (this.data.start < this.data.total) {
+      if (this.data.isSearch) {
+        util.http(url + '?keywords=' + this.data.keywords + 'start=' + this.data.start + '&count=' + this.data.count, this.morePostList);
+      } else {
+        util.http(url + '?start=' + this.data.start + '&count=' + this.data.count, this.morePostList);
+      }
+    } else {
       this.setData({
+        isComplete: true,
+        loadTips: app.globalData.COMPLETE
+      })
+    }
+  },
+  morePostList: function (res) {
+    var totalPost = {};
+
+    if (this.data.isSearch) {
+      totalPost = this.data.searchList.concat(res.data.postData);
+      this.setData({
+        searchList: totalPost,
         hasMore: false
       })
-    }, 2000)
+    } else {
+      totalPost = this.data.postList.concat(res.data.postData);
+      this.setData({
+        postList: totalPost,
+        hasMore: false
+      })
+    }
   },
   onTap: function (evt) {
     var id = evt.currentTarget.dataset.id;
@@ -74,27 +157,24 @@ Page({
   },
   onBindFocus: function (evt) {
     this.setData({
-      isShow: true
+      isSearch: true,
+      start: 0,
+      isComplete: false,
+      hasMore: false,
+      loadTips: app.globalData.LOADING,
     })
   },
   onCancel: function (evt) {
     this.setData({
-      isShow: false,
+      isSearch: false,
       searchValue: '',
       searchList: '',
       searchHotShow: true
     })
   },
-  onBindBlur: function () {
-    util.http(app.indexAPI.post, this.getSearchList)
-  },
-  onNav: function(evt) {
-    var index = evt.currentTarget.dataset.id;
-    if(this.data.navcur == index) return;
-    this.setData({
-      navcur: index
-    })
-    util.http(app.indexAPI.post + '?nav=' + this.data.navcur, this.getPostList)
+  onBindBlur: function (evt) {
+    this.data.keywords = evt.detail.value;
+    util.http(app.indexAPI.search + '?&keywords=' + this.data.keywords + 'start=0&count=' + this.data.count, this.getSearchList)
   },
   getSearchList: function (res) {
     wx.hideLoading();
